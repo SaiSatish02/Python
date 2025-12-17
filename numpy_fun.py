faa6998f398c876e9dc142de4b9842e4ef444ad4
SaiSatish02 <kssaisatish@gmail.com>
2025-12-17 18:36:08 +0530

    Add files via upload

---
+++ numpy_fun.py
@@ -0,0 +1,10 @@
+import numpy as np
+A = np.array([[8,4,3],
+              [7,3,9],
+              [2,5,6]])
+traceA = np.trace(A)
+print("Matrix is:")
+print(A)
+
+print("\nTrace of the matrix:")
+print(traceA)
